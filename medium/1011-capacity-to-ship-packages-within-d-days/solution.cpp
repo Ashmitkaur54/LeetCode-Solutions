627b87bfb136715256a044c6064c9a4a518b92cf
Ashmitkaur54 <ashmit.hora05@gmail.com>
2026-08-09 17:47:19 +0530

    ✅ Capacity To Ship Packages Within D Days | Medium | C++ | Runtime: 15 ms (29.61%) | Memory: 35.1 MB | 2026-08-09

--- medium/1011-capacity-to-ship-packages-within-d-days/solution.cpp
+++ medium/1011-capacity-to-ship-packages-within-d-days/solution.cpp
@@ -1,7 +1,30 @@
-            for(int i=0;i<weights.size();i++){
-                if(weights[i]+load<=mid){
-                    load+=weights[i];
-                }
-                else{
-                    d++;
-                    load=weights[i];
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low=*max_element(weights.begin(),weights.end());
+        int high=accumulate(weights.begin(),weights.end(),0);
+        int ans=high;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int load=0;
+            int d=1;
+            for(int i=0;i<weights.size();i++){
+                if(weights[i]+load<=mid){
+                    load+=weights[i];
+                }
+                else{
+                    d++;
+                    load=weights[i];
+                }
+            }
+            if(d<=days){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
