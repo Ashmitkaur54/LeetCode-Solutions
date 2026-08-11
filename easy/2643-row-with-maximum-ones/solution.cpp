46917f865117092c911b029db52cc96e6aa1ae31
Ashmitkaur54 <ashmit.hora05@gmail.com>
2026-08-12 00:40:05 +0530

    ✅ Row With Maximum Ones | Easy | C++ | Runtime: 2 ms (65.91%) | Memory: 64.8 MB | 2026-08-11

--- easy/2643-row-with-maximum-ones/solution.cpp
+++ easy/2643-row-with-maximum-ones/solution.cpp
@@ -1,5 +1,18 @@
-class Solution {
+class Solution {
 public:
-    vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
-        int maxi=0;
-        int index=0;
+    vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+        int maxi=0;
+        int index=0;
+        for(int i=0;i<mat.size();i++){
+            int count=0;
+            for(int j=0;j<mat[0].size();j++){
+                count+=mat[i][j];
+            }
+            if(maxi<count){
+                maxi=count;
+                index=i;
+            }
+        }
+        return {index,maxi};
+    }
+};
