d7b6109abcc72d02267e0d17e0a5458cee55a74a
Ashmitkaur54 <ashmit.hora05@gmail.com>
2026-08-12 23:46:50 +0530

    ✅ Self Dividing Numbers | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 9.1 MB | 2026-08-12

--- easy/0728-self-dividing-numbers/solution.cpp
+++ easy/0728-self-dividing-numbers/solution.cpp
@@ -1,3 +1,28 @@
-    vector<int> selfDividingNumbers(int left, int right) {
-class Solution {
+class Solution {
 public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> ans;
+
+        for(int num = left; num <= right; num++) {
+            int n = num;
+            bool flag = true;
+
+            while(n > 0) {
+                int digit = n % 10;
+
+                if(digit == 0 || num % digit != 0) {
+                    flag = false;
+                    break;
+                }
+
+                n = n / 10;
+            }
+
+            if(flag) {
+                ans.push_back(num);
+            }
+        }
+
+        return ans;
+    }
+};
