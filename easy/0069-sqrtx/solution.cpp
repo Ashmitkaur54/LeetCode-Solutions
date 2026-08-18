e0f131861fee5492e4b22ab30bbca65cc4ca2199
Ashmitkaur54 <ashmit.hora05@gmail.com>
2026-08-18 21:56:52 +0530

    ✅ Sqrt(x) | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 8.6 MB | 2026-08-18

--- easy/0069-sqrtx/solution.cpp
+++ easy/0069-sqrtx/solution.cpp
@@ -1,6 +1,19 @@
-class Solution {
+class Solution {
 public:
-    int mySqrt(int x) {
-        int low=0;
-        int high=x;
-        int ans=0;
+    int mySqrt(int x) {
+        int low=0;
+        int high=x;
+        int ans=0;
+        while(low<=high){
+            int mid=1LL*(low+high)/2;
+            if(1LL*mid*mid<=x){
+                ans=1LL*max(ans,mid);
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
