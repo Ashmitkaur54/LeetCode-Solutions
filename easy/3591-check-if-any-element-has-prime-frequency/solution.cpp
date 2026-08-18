ccbce66c82c12f56a859a4ba9e4c9b8629652b14
Ashmitkaur54 <ashmit.hora05@gmail.com>
2026-08-18 22:14:53 +0530

    ✅ Check if Any Element Has Prime Frequency | Easy | C++ | Runtime: 0 ms | Memory: 8 MB | 2026-08-18

--- easy/3591-check-if-any-element-has-prime-frequency/solution.cpp
+++ easy/3591-check-if-any-element-has-prime-frequency/solution.cpp
@@ -1,9 +1,7 @@
         for(int i=0;i<nums.size();i++){
             if(nums[i]==1){
                 ans=true;
-            }
-            for(int j=i+1;j*j<=i;j++){
-                if(i%j==0){
-                return 0;
-                    ans=false;
-                    break;
+        bool ans=true;
+    bool checkPrimeFrequency(vector<int>& nums) {
+public:
+class Solution {
