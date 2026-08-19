47cb39f1ffe395853218e9d335fbfb0c2e3aecd7
Ashmitkaur54 <ashmit.hora05@gmail.com>
2026-08-19 11:44:47 +0530

    ✅ Prime In Diagonal | Easy | C++ | Runtime: 4 ms (55.04%) | Memory: 39.7 MB | 2026-08-19

--- easy/2614-prime-in-diagonal/solution.cpp
+++ easy/2614-prime-in-diagonal/solution.cpp
@@ -1,9 +1,36 @@
-        for(int i=2;i*i<=x;i++){
-            if(x%i==0){
-                return false;
-            }
-        }
-        return true;
-    }
-    int diagonalPrime(vector<vector<int>>& nums) {
-        int k=0;
+class Solution {
+public:
+    bool prime(int x){
+        if(x<2){
+            return false;
+        }
+        for(int i=2;i*i<=x;i++){
+            if(x%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    int diagonalPrime(vector<vector<int>>& nums) {
+        int k=0;
+        int l=0;
+        int maxi=0;
+        while(k<nums.size() && l<nums[0].size()){
+            if(prime(nums[k][l])==true){
+                maxi=max(maxi,nums[k][l]);
+            }
+            k++;
+            l++;
+        }
+        k=0;
+        l=nums[0].size()-1;
+        while(k<nums.size() && l>=0){
+            if(prime(nums[k][l])==true){
+                maxi=max(maxi,nums[k][l]);
+            }
+            k++;
+            l--;
+        }
+        return maxi;
+    }
+};
