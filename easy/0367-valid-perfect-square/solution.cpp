92d548555f4dffd0b9c24a978e97696cb946d9e3
Ashmitkaur54 <ashmit.hora05@gmail.com>
2026-08-20 12:00:37 +0530

    ✅ Valid Perfect Square | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 7.8 MB | 2026-08-20

--- easy/0367-valid-perfect-square/solution.cpp
+++ easy/0367-valid-perfect-square/solution.cpp
@@ -1,9 +1,22 @@
-class Solution {
+class Solution {
 public:
-    bool isPerfectSquare(int num) {
-        int low=0;
-        int high=num;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(mid*mid==num){
-        int ans=0;
+    bool isPerfectSquare(int num) {
+        int low=0;
+        int high=num;
+        bool ans=false;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(1LL*mid*mid==num){
+                ans=true;
+                break;
+            }
+            else if(1LL*mid*mid<num){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
