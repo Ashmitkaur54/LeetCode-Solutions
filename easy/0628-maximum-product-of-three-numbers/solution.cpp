6ddaa7edca27a208cd82142c1ecf120c316375b6
Ashmitkaur54 <ashmit.hora05@gmail.com>
2026-08-22 22:59:25 +0530

    ✅ Maximum Product of Three Numbers | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 31.6 MB | 2026-08-22

--- easy/0628-maximum-product-of-three-numbers/solution.cpp
+++ easy/0628-maximum-product-of-three-numbers/solution.cpp
@@ -1,9 +1,10 @@
+class Solution {
 public:
-    int maximumProduct(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        int num=max(nums[n-1]*nums[n-2]*nums[n-3],nums[0]*nums[1]*nums[2]);
-        return num;
-    }
-    
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int num=max(nums[n-1]*nums[n-2]*nums[n-3],nums[0]*nums[1]*nums[n-1]);
+        return num;
+    }
+    
 };
