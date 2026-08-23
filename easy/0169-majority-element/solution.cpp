1e6e40b64821ce17a275f6845a9746a9b53d2010
Ashmitkaur54 <ashmit.hora05@gmail.com>
2026-08-23 13:58:38 +0530

    ✅ Majority Element | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 28.2 MB | 2026-08-23

--- easy/0169-majority-element/solution.cpp
+++ easy/0169-majority-element/solution.cpp
@@ -1,4 +1,20 @@
-        int el=0;
-        int cnt=0;
-        for(int i=0;i<nums.size();i++){
-            if(cnt==0){
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int el=0;
+        int cnt=0;
+        for(int i=0;i<nums.size();i++){
+            if(cnt==0){
+                el=nums[i];
+                cnt=1;
+            }
+            else if(el==nums[i]){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+        return el;
+    }
+};
