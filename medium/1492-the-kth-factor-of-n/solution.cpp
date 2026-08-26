cb13058b18d8bef5e5e1a51198acf70d1d73c88c
Ashmitkaur54 <ashmit.hora05@gmail.com>
2026-08-26 11:54:45 +0530

    ✅ The kth Factor of n | Medium | C++ | Runtime: 0 ms | Memory: 7.8 MB | 2026-08-26

--- medium/1492-the-kth-factor-of-n/solution.cpp
+++ medium/1492-the-kth-factor-of-n/solution.cpp
@@ -1,6 +1,7 @@
-class Solution {
-public:
-    int kthFactor(int n, int k) {
-        vector<int>v;
         for(int i=1;i*i<=n;i++){
             if(n%i==0){
+                v.push_back(i);
+                if(i!=n/i){
+                    v.push_back(n/i);
+                }
+            }
