6a9b3025ce736749bb3017b513b9f91170b7891f
Ashmitkaur54 <ashmit.hora05@gmail.com>
2026-08-31 11:39:47 +0530

    ✅ Sum Multiples | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 8.4 MB | 2026-08-31

--- easy/2652-sum-multiples/solution.cpp
+++ easy/2652-sum-multiples/solution.cpp
@@ -1,6 +1,18 @@
-           if(i%3==0){
-        for(int i=1;i<=n;i++){
-        int sum=0;
-    int sumOfMultiples(int n) {
+class Solution {
 public:
-class Solution {
+    int sumOfMultiples(int n) {
+        int sum=0;
+        for(int i=1;i<=n;i++){
+           if(i%3==0){
+            sum+=i;
+           }
+           else if(i%5==0){
+            sum+=i;
+           }
+           else if(i%7==0){
+            sum+=i;
+           }
+        }
+        return sum;
+    }
+};
