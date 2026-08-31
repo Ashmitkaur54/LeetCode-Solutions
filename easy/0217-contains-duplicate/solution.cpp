291ce87f364d6c5ba404e5718b6c8b93b073531e
Ashmitkaur54 <ashmit.hora05@gmail.com>
2026-08-31 11:54:28 +0530

    ✅ Contains Duplicate | Easy | C++ | Runtime: 98 ms (11.73%) | Memory: 113.4 MB | 2026-08-31

--- easy/0217-contains-duplicate/solution.cpp
+++ easy/0217-contains-duplicate/solution.cpp
@@ -1,1 +1,16 @@
-class Solution {
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;;
+        }
+        bool ans=false;
+        for(auto k:mp){
+            if(k.second>=2){
+                ans=true;
+            }
+        }
+        return ans;
+    }
+};
