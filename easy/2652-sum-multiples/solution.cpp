f78c7a920c7c603d2551dc0902529852806e3981
Ashmitkaur54 <ashmit.hora05@gmail.com>
2026-08-31 11:36:59 +0530

    ✅ Sum Multiples | Easy | C++ | Runtime: 0 ms | Memory: 7.9 MB | 2026-08-31

--- easy/2652-sum-multiples/solution.cpp
+++ easy/2652-sum-multiples/solution.cpp
@@ -1,8 +1,6 @@
-            while(i%3==0){
+           if(i%3==0){
         for(int i=1;i<=n;i++){
         int sum=0;
     int sumOfMultiples(int n) {
 public:
-                n/=3;
-            }
-            while(i%5==0){
+class Solution {
