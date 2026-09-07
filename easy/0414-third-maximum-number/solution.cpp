e708b7e0ece4d6409456782adfe9c1ff1ff4dd0a
Ashmitkaur54 <ashmit.hora05@gmail.com>
2026-09-07 23:23:20 +0530

    ✅ Third Maximum Number | Easy | C++ | Runtime: 3 ms (23.79%) | Memory: 14.5 MB | 2026-09-07

--- easy/0414-third-maximum-number/solution.cpp
+++ easy/0414-third-maximum-number/solution.cpp
@@ -1,7 +1,16 @@
-class Solution {
+class Solution {
 public:
-    int thirdMax(vector<int>& nums) {
-        set<int>st;
-        for(int i=0;i<nums.size();i++){
-            st.insert(nums[i]);
-        }
+    int thirdMax(vector<int>& nums) {
+        set<int>st;
+        for(int i=0;i<nums.size();i++){
+            st.insert(nums[i]);
+        }
+        if(st.size()<3){
+            return *st.rbegin();
+        }
+        auto it=st.rbegin();
+        it++;
+        it++;
+        return *it;
+    }
+};
