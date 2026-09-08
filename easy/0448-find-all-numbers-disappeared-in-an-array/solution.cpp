88931ff53f22ffa47d7b228d3e1c6135adea7ca1
Ashmitkaur54 <ashmit.hora05@gmail.com>
2026-09-08 23:25:49 +0530

    ✅ Find All Numbers Disappeared in an Array | Easy | C++ | Runtime: 39 ms (29.25%) | Memory: 64.2 MB | 2026-09-08

--- easy/0448-find-all-numbers-disappeared-in-an-array/solution.cpp
+++ easy/0448-find-all-numbers-disappeared-in-an-array/solution.cpp
@@ -1,5 +1,16 @@
-        for(int k:nums){
-        unordered_set<int>st;
-    vector<int> findDisappearedNumbers(vector<int>& nums) {
+class Solution {
 public:
-class Solution {
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        unordered_set<int>st;
+        for(int k:nums){
+            st.insert(k);
+        }
+        vector<int>v;
+        for(int i=1;i<=nums.size();i++){
+            if(st.find(i)==st.end()){
+                v.push_back(i);
+            }
+        }
+        return v;
+    }
+};
