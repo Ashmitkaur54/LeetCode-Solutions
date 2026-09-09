70347138c00df24fc947ef343ad060939abcfc42
Ashmitkaur54 <ashmit.hora05@gmail.com>
2026-09-09 11:59:51 +0530

    ✅ Count Primes | Medium | C++ | Runtime: 0 ms | Memory: 7.8 MB | 2026-09-09

--- medium/0204-count-primes/solution.cpp
+++ medium/0204-count-primes/solution.cpp
@@ -1,8 +1,26 @@
-        for(int j=2;j*j<=a;j++){
-        }
-            ans=false;
-        if(a<2){
-        bool ans=true;
-    bool isprime(int a){
+class Solution {
 public:
-class Solution {
+    bool isprime(int a){
+        bool ans=true;
+        if(a<2){
+            ans=false;
+        }
+        for(int j=2;j*j<=a;j++){
+            if(a%j==0){
+                ans=false;
+                break;
+            }
+        }
+        return ans;
+    }
+    int countPrimes(int n) {
+        int count=0;
+        for(int i=2;i<=n;i++){
+            if(isprime(i)){
+                count++;
+            }
+            
+        }
+        return count;
+    }
+};
