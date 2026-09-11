726ae4b2730b0563ec3ce81b116d03c5d1ff6373
Ashmitkaur54 <ashmit.hora05@gmail.com>
2026-09-11 23:19:15 +0530

    ✅ Longest Continuous Increasing Subsequence | Easy | C++ | Runtime: 0 ms | Memory: 8.1 MB | 2026-09-11

---
+++ easy/0674-longest-continuous-increasing-subsequence/solution.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        int count=0;
+        int maxi=0;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i-1]<nums[i]){
