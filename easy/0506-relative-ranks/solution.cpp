4a631536da9d3e2f8445392db73404db6800e356
Ashmitkaur54 <ashmit.hora05@gmail.com>
2026-09-12 22:59:19 +0530

    ✅ Relative Ranks | Easy | C++ | Runtime: 0 ms | Memory: 8.3 MB | 2026-09-12

--- easy/0506-relative-ranks/solution.cpp
+++ easy/0506-relative-ranks/solution.cpp
@@ -1,6 +1,3 @@
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
-        vector<string>v;
-        for(int i=0;i<score.size();i++){
-            if(score[i]==5){
